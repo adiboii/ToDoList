9eb55faa5b482f0f10b98edec9d6a8b0941141e0
adi.boyii <adrianpauln.reyes@gmail.com>
2022-07-05 09:09:10 +0800

    Add project files.

---
+++ ToDoList/ToDoList/MainModelView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ToDoList
+{
+    internal class MainModelView : INotifyPropertyChanged
+    {
+
+        
+        
+        
+
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged()
+        {
+
+        }
+    }
+}
---
+++ ToDoList/ToDoList/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDoList
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void newTaskHandler(object sender, TextChangedEventArgs args)
+        {
+            Console.WriteLine(args.NewTextValue);
+        }
+    }
+}
